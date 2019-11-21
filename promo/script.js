59f074373777586043a82eb659b3d5999dd73968
Денис Евдокимов <denisevdokimov@MacBook-Pro-Denis.local>
2019-11-21 23:59:30 +0300

    13 урок (основное)

--- promo/script.js
+++ promo/script.js
@@ -1,12 +1,6 @@
 let now = new Date(),
     hrs = now.getHours(),
-    minutes = now.getMinutes(),
-    seconds = now.getSeconds(),
-    day = now.getDay(),
     msg = '',
-    dayOfWeeck = '',
-    amPm = '',
-    hours,
     timerDay;
 
 
@@ -19,51 +13,8 @@ if (hrs < 12) {
 }  
 
 console.log(msg);
-
-if (day == 1) {
-  day = "Понедельник"; 
-} else if (day == 2) {
-  day = "Вторник"; 
-} else if (day == 3) {
-  day = "Среда";
-} else if (day == 4) {
-  day = "Четверг";
-} else if (day == 5) {
-  day = "Пятница";
-} else if (day == 6) {
-  day = "Суббота";
-} else if (day == 7) {
-  day = "Восскресенье";
-}
-
-console.log('Сегодня: ' + day);
-
-
-
-if (minutes < 10) {
-  minutes = '0' + minutes;
-} 
-if (seconds < 10) {
-  seconds = '0' + seconds;
-} 
-if (hrs < 10) {
-  hrs = '0' + hrs;
-} 
-
-if (hrs >= 12) {
-  amPm = 'pm';
-  hours = hrs - 12;
-  console.log(hours);
-} else if (hrs < 12) {
-    amPm = 'am';
-    hours = hrs;
-}
-
-
-
-
- 
-console.log('Текущее время:' + ' ' + hours + ':' + minutes + ':' + seconds + ' ' + amPm);
+console.log('Сегодня: ' + now.toLocaleString('ru', {weekday: 'long'}));
+console.log('Текущее время:' + ' ' + now.toLocaleTimeString('en'));
 
 let timerCount = function(deadline) {
   
--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
-  let timerCount = function(deadline) {
+  // Тймер 
+  const timerCount = function(deadline) {
     let timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
@@ -20,50 +21,105 @@ window.addEventListener('DOMContentLoaded', function() {
     function updateClock () {
       let timer = getTimeRemaining();
 
-      // function addZero (i) {
-      //   if (i < 10 ) {
-      //     i = '0' + i;
-      //     return i;
-      //   }
-      // }
-      // console.log(addZero(timer.seconds));
-
-      // addZero(timer.seconds);
-      // addZero(timer.minutes);
-      // addZero(timer.hours);
-
-      if (timer.seconds < 10) {
-        timer.seconds = '0' + timer.seconds;
-      }
-      if (timer.minutes < 10) {
-        timer.minutes = '0' + timer.minutes;
-      }
-      if (timer.hours < 10) {
-        timer.hours = '0' + timer.hours;
+      function addZero (i) {
+        if (i < 10 ) {
+          i = '0' + i;
+        }
+        return i;
       }
 
+      timerSeconds.textContent = addZero(timer.seconds);
+      timerMinutes.textContent = addZero(timer.minutes);
+      timerHours.textContent = addZero(timer.hours); 
 
-      
-      timerSeconds.textContent = timer.seconds;
-      timerMinutes.textContent = timer.minutes;
-      timerHours.textContent = timer.hours; 
-
-      
-      let timerId = 0;
-      if (timer.timeRemaining > 0) { 
-        timerId = setInterval(updateClock, 1000);
-      } else {
+      if (timer.timeRemaining <= 0) { 
+        
         clearInterval(timerId);
         timerSeconds.textContent = '00';
         timerMinutes.textContent = '00';
         timerHours.textContent = '00'; 
       }
     }
-
-       updateClock();  
+    
+    let timerId = setInterval(updateClock, 1000);
+      
   };
-  console.log('привет');
+  timerCount('20 november 2019 23:40:00');
 
-  timerCount('19 november 2019 22:49:00');
+
+  // Меню
+  const toggleMenu = () => {
+
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          closeBtn = document.querySelector('.close-btn'),
+          menuItems = menu.querySelectorAll('ul>li');
+    
+    const handlerMenu = () => {
+      if (!menu.style.transform || menu.style.transform === `translateX(-100%)`) {
+        menu.style.transform = `translate(0)`;
+      } else {
+        menu.style.transform = `translateX(-100%)`;
+      }
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+  };
+  toggleMenu();
+
+
+  //ПопАп (модалка)
+  const togglePopUp = () => {
+
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close'),
+          widthWindow = document.documentElement.clientWidth;
+          
+
+    popupBtn.forEach((item) => {
+      item.addEventListener('click', () => {
+        popup.style.display = 'block';
+        // if (widthWindow > 768) {
+        //   popupOpenShow();
+        // }
+        popupOpenShow();
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popupCloseShow();
+    }); 
+
+    let step1 = 0;
+    const popupOpenShow = () => {
+      
+      step1 += 0.05;
+      popup.style.opacity = `${step1}`; 
+      
+      if (step1 < 1) {
+        setTimeout(popupOpenShow, 20);
+      }      
+    };
+
+
+    let step2 = 1;
+    const popupCloseShow = () => {
+      
+      step2 -= 0.05;
+      popup.style.opacity = `${step2}`; 
+        
+      if (step2 > 0) {
+        setTimeout(popupCloseShow, 20);
+      } else if (step2 === 0) {
+        clearTimeout(popupCloseShow);
+        popup.style.display = 'none';
+      }
+      
+    };
+  };
+  togglePopUp();
 
 });
